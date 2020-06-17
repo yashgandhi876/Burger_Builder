d69a8f747cd61f9c19aabcbaf08fab84432ad598
Yash Gandhi <yashgandhi2020@gmail.com>
2020-06-17 15:16:56 +0530

    Updates

---
+++ precache-manifest.d75dcb7ea166f639a1e79ae32f6a459f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593542b3276d929d7ea31bb6b64d31df",
+    "url": "/Burger_Builder/index.html"
+  },
+  {
+    "revision": "ce740abdee9f2ac83d81",
+    "url": "/Burger_Builder/static/css/main.e44b4e34.chunk.css"
+  },
+  {
+    "revision": "2d16011f3be81f9ce8e7",
+    "url": "/Burger_Builder/static/js/2.18aecc15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger_Builder/static/js/2.18aecc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce740abdee9f2ac83d81",
+    "url": "/Burger_Builder/static/js/main.bfcf60cf.chunk.js"
+  },
+  {
+    "revision": "e7f947e5d941f9aff170",
+    "url": "/Burger_Builder/static/js/runtime-main.93d08e60.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger_Builder/static/media/burgerLogo.b8503d26.png"
+  }
+]);
