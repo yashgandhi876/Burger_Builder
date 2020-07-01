03256cbf37fcf865d23a36f4a8bf3d31f5b8072a
yashgandhi876 <yashgandhi2020@gmail.com>
2020-07-01 15:23:40 +0530

    added basepath

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import {BrowserRouter} from "react-router-dom"
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename="/Burger_Builder">
   <App />
   </BrowserRouter>
 )
